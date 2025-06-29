4e513fd6eccb7e40280b2e0b78bcdf48fb274b85
Harisharnam <168778202+harisharnamm@users.noreply.github.com>
2025-06-29 16:52:08 +0530

    Fix useEffect import in Clients component

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useClients } from '../hooks/useClients';
 import { TopBar } from '../components/organisms/TopBar';
